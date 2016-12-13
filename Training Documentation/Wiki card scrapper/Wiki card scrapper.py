067daa96e33ecd8b66a8926648ac249cc6ea6441
Jeff Falberg <auronzeroo@aim.com>
2016-12-12 20:09:52 -0500

    Wiki scrapper can now search for prescript cards

--- Training Documentation/Wiki card scrapper/Wiki card scrapper.py
+++ Training Documentation/Wiki card scrapper/Wiki card scrapper.py
@@ -17,10 +17,23 @@ query = open(output_file, "w", encoding='utf-8')
 listoflines = database.readlines()
 database.close()
 listofdata = []
+prescript = {'VJMP-JP': 200,
+             'WJMP-JP': 203,
+             'SJMP-JP': 204,
+             '20AP-JP': 213,
+             'PP19-JP': 214,
+             'SD31-JP': 331,
+             'MACR-JP': 912}
 
 for line in listoflines :
+    old_line = line
     while(len(line)<9) :
         line = '0' + line
+    if len(line)>9 :
+        pack = int(line[3:6])
+        number = line[6:9]
+        pack = list(prescript.keys())[list(prescript.values()).index(pack)]
+        line = str(pack) + number
     wiki_url = "http://yugioh.wikia.com/wiki/" + line
     try:
         page = urr.urlopen(wiki_url)
@@ -31,21 +44,21 @@ for line in listoflines :
             patternName = re.compile(regexName)
             card_name = re.findall(patternName, source)[0]
             card_name = re.sub('"', '""', card_name)
-            card_name = re.sub("'", "''", card_name)
+            #regexText = re.compile(r";;\">\n(.*[^<\/td>]*\.)*<\/td>", re.S)
             regexText = re.compile(r";;\">\n(.*)")
             patternText = re.compile(regexText)
             card_text = re.findall(patternText, source)[0]
             card_text = re.sub('(?!<dd>|</dd>|<dl>|</dl>|<br>|<br />)(<.*?>)','', card_text)
             card_text = re.sub('"', '""', card_text)
-            card_text = re.sub("'", "''", card_text)
             card_text = re.sub('<dd>', '\n', card_text)
             card_text = re.sub('</dd>', '\n', card_text)
             card_text = re.sub('<dl>', '\n', card_text)
             card_text = re.sub('</dl>', '\n', card_text)
             card_text = re.sub('<br />', '\n', card_text)
+            card_text = re.sub('&amp;', '&', card_text)
             print('Processing: '+line)
             #print('update texts set name="'+card_name+'", desc="'+card_text+'" where id='+line+';')
-            query.write('update texts set name="'+card_name+'", desc="'+card_text+'" where id='+line+';')
+            query.write('update texts set name="'+card_name+'", desc="'+card_text+'" where id='+old_line+';')
     except urllib.error.HTTPError as err :
         if err.code == 404 :
             continue
